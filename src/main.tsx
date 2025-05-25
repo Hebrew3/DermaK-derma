4b4022fae9003979e9f9086bdbd5079c4835fe32
Hebrew3 <ddeasis1990@gmail.com>
2025-05-25 13:28:11 +0800

    s

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import '@/assets/styles/globals.css'
 import '@/styles/globals.css'
-// import AppRouter from '@/routes/AppRouter'.
+// import AppRouter from '@/routes/AppRouter'
 import App from './App'
 
 createRoot(document.getElementById('root')!).render(
